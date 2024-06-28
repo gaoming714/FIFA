008c9a5e6a3cb54f000073ce8abc3b8ff61a05c5
gaoming714 <gaoming714@126.com>
2024-06-28 16:15:35 +0800

    rename base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+    base: '/FIFA/',
   lang: 'en-US',
 
   title: 'VuePress',
